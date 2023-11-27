c0986c8196bc5b23c78efb5896603001fad760a3
rain <gujjang888@gmail.com>
2023-11-27 05:23:18 +0000

    rain

--- miniProvisioning/adminapp/urls.py
+++ miniProvisioning/adminapp/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 from .views import home, success
 from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+from .views import CustomLoginView, test
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('test/', views.home, name='test'),
     path('signup/', views.signup, name='signup'),
     path('success/', success, name='success'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('start_ec2_instance/', views.start_ec2_instance, name='start_ec2_instance'),
 ]
--- miniProvisioning/adminapp/views.py
+++ miniProvisioning/adminapp/views.py
@@ -2,16 +2,27 @@ import boto3
 import os
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
 from decouple import config
 from .forms import MemberForm
 from .models import Member
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.urls import reverse_lazy
 
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    success_url = reverse_lazy('test')
 
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password. Please try again.')
+        return super().form_invalid(form)
+
+@login_required
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'test.html')
 
 def start_ec2_instance(request):
     # AWS 자격 증명 설정
@@ -52,24 +63,14 @@ def signup(request):
             folder_path = os.path.join('index', folder_name)
             os.makedirs(folder_path)
 
-            return redirect('success')  # 회원 가입 성공 페이지로 리다이렉트
+            return redirect('home')  # 회원 가입 성공 시 test 페이지로 리다이렉트
     else:
         form = MemberForm()
 
     return render(request, 'signup.html', {'form': form})
 
 def success(request):
-    # 회원 가입 성공 후 로그인 처리
-    if request.method == 'POST':
-        username = request.POST['id']  # 사용자가 입력한 아이디 필드 이름에 맞게 수정
-        password = request.POST['password']  # 사용자가 입력한 비밀번호 필드 이름에 맞게 수정
-        user = authenticate(request, username=username, password=password)
+    return render(request, 'success.html')  # success.html 템플릿 파일을 렌더링하도록 변경
 
-        if user is not None:
-            login(request, user)
-            return redirect('login')  # 로그인이 성공하면 로그인 페이지로 이동
-        else:
-            # 로그인 실패 시 원하는 처리 추가
-            pass
-
-    return render(request, 'login.html')
+def test(request):
+    return render(request, 'test.html')
