f50ff8612d284f32376d0e536fb484d51d7dca74
rain <gujjang888@gmail.com>
2023-11-27 01:00:19 +0000

    rain

--- miniProvisioning/adminapp/apps.py
+++ miniProvisioning/adminapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AdminappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'adminapp'
+
+    def ready(self):
+            import adminapp.signals  # 신호 핸들러 등록
