59ada774aebdc298665dd0ddb3bb05edfd85f292
orthia194 <qscxza1231@naver.com>
2023-11-27 21:00:27 +0900

    v.1.23

--- miniProvisioning/adminapp/urls.py
+++ miniProvisioning/adminapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('test/', views.home, name='test'),
-    # path('signup/', views.signup, name='signup'),
+    path('instance_list/', views.list_ec2_instances, name='instance_list'),
     path('start_ec2_instance/', views.start_ec2_instance, name='start_ec2_instance'),
 ]
--- miniProvisioning/adminapp/views.py
+++ miniProvisioning/adminapp/views.py
@@ -25,11 +25,20 @@ def start_ec2_instance(request):
 
     # EC2 인스턴스 시작 요청
     response = ec2.run_instances(
-        ImageId='AMI_ID',  # 사용할 AMI ID
+        ImageId='ami-01123b84e2a4fba05',  # 사용할 AMI ID
         MinCount=1,
         MaxCount=1,
-        InstanceType='INSTANCE_TYPE',  # 인스턴스 유형 (예: 't2.micro')
-        KeyName='KEY_PAIR_NAME'  # EC2 인스턴스에 연결할 키페어 이름
+        InstanceType='t2.micro',  # 인스턴스 유형 (예: 't2.micro')
+        KeyName='admin',  # EC2 인스턴스에 연결할 키페어 이름
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'Name', 'Value': 'orthia-test'},  # 인스턴스 이름 지정
+                    {'Key': 'Customer', 'Value': '홍길동'}  # 고객 정보 태그 추가
+                ]
+            }
+        ]
     )
 
     # 생성된 인스턴스 ID 가져오기
@@ -37,13 +46,30 @@ def start_ec2_instance(request):
 
     return HttpResponse(f"EC2 인스턴스가 시작되었습니다. 인스턴스 ID: {instance_id}")
 
-def signup(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # 회원가입 후 리다이렉트할 페이지
-    else:
-            form = CustomUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+def list_ec2_instances(request):
+    # AWS 자격 증명 설정
+    aws_access_key = config('your_aws_access_key')
+    aws_secret_key = config('your_aws_secret_key')
+    region_name = 'ap-northeast-2'
+
+    # AWS 자격 증명을 사용하여 Boto3 클라이언트 생성
+    ec2 = boto3.client(
+        'ec2',
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key,
+        region_name=region_name
+    )
+
+    # EC2 인스턴스 목록 가져오기
+    instances = ec2.describe_instances()
+    
+ # 각 인스턴스의 이름 가져오기
+    instance_names = []
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            for tag in instance.get('Tags', []):
+                if tag['Key'] == 'Name':
+                    instance_names.append(tag['Value'])
+
+    # 인스턴스 목록과 인스턴스 이름들을 HTML에 전달
+    return render(request, 'instance_list.html', {'instances': instances, 'instance_names': instance_names})
