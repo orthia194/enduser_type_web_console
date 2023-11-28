8aa62ffb981495e0e2ba8b70cb96b5e9d5b0078b
imahost <gujjang888@gmail.com>
2023-11-28 10:54:05 +0900

    rain

--- miniProvisioning/adminapp/utils.py
+++ miniProvisioning/adminapp/utils.py
@@ -1,14 +1,14 @@
 # utils.py
 
 from django.http import JsonResponse
-from django.contrib.auth.models import User
+from .models import Member
 
 def check_existing_id(request):
     id = request.GET.get('id', '')
-    exists = User.objects.filter(username=id).exists()
+    exists = Member.objects.filter(id=id).exists()
     return JsonResponse({'exists': exists})
 
 def check_existing_email(request):
     email = request.GET.get('email', '')
-    exists = User.objects.filter(email=email).exists()
+    exists = Member.objects.filter(email=email).exists()
     return JsonResponse({'exists': exists})
