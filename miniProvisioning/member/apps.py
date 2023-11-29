a5666afa0471a4ab1d43c38aef0b8fb378cb2800
rain <gujjang888@gmail.com>
2023-11-29 00:57:22 +0000

    rain

--- miniProvisioning/member/apps.py
+++ miniProvisioning/member/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AdminappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adminapp'
+    name = 'member'
 
     def ready(self):
-        import adminapp.signals
+        import member.signals
